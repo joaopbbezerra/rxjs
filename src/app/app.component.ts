fbfdbce43ab944d6967e7f55ae4848c1b6e09ed7
João Pedro Bezerra <joaopbbezerra@gmail.com>
2023-04-09 18:58:17 +0100

    first promise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'rxjs';
+
+
+  ngOnInit(): void {
+    this.myPromise('Eduardo').then(result => console.log(result))
+    this.myPromise('João').then(result => console.log(result)).catch(error => console.log('Error: ', error))
+  }
+
+  myPromise(name: string):Promise<string>{
+    return new Promise((resolve, reject) => {
+      if (name === 'Eduardo'){
+        setTimeout(()=> {
+          resolve(`Welcome, ${name}`)
+        }, 1000)
+      } else {
+        reject(`Ops, you're not ${name}`)
+      }
+    })
+  }
 }
